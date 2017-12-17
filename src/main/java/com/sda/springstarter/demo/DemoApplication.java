b7eebcdfd1643cbc97b53b2068b48d82d8a96924
AhlenSDA <ahlensda@wp.pl>
2017-12-17 13:55:27 +0100

    Running application - success

--- src/main/java/com/sda/springstarter/demo/DemoApplication.java
+++ src/main/java/com/sda/springstarter/demo/DemoApplication.java
@@ -1,12 +1,17 @@
 package com.sda.springstarter.demo;
 
 import com.sda.springstarter.demo.model.Book;
+import com.sda.springstarter.demo.service.BookServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication implements CommandLineRunner {
+
+    @Autowired
+    private BookServiceImpl bookService;
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
@@ -17,6 +22,9 @@ public class DemoApplication implements CommandLineRunner {
 
         Book book1 = new Book("Title1", "Author1");
         Book book2 = new Book("Title2", "Author2");
+
+        bookService.saveBook(book1);
+        bookService.saveBook(book2);
 
     }
 }
