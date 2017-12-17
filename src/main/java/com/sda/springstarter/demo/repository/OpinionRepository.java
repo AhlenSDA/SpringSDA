20eeb060debc4373539b6571a4947c8ccb7a4ebd
AhlenSDA <ahlensda@wp.pl>
2017-12-17 14:14:26 +0100

    Opinion and Shop repository added

---
+++ src/main/java/com/sda/springstarter/demo/repository/OpinionRepository.java
@@ -0,0 +1,10 @@
+package com.sda.springstarter.demo.repository;
+
+import com.sda.springstarter.demo.model.Opinion;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OpinionRepository extends JpaRepository<Opinion, Long> {
+
+
+
+}
