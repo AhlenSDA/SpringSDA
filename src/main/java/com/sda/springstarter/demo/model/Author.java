c392d749d22c06b3abb3fd452d00f5103a4bf338
AhlenSDA <ahlensda@wp.pl>
2018-01-13 09:59:06 +0100

    Add classes Publisher, Author, Category

---
+++ src/main/java/com/sda/springstarter/demo/model/Author.java
@@ -0,0 +1,59 @@
+package com.sda.springstarter.demo.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String address;
+    private String lastName;
+    private String name;
+
+    public Author() {
+    }
+
+    public Author(String address, String lastname, String name) {
+        this.address = address;
+        this.lastName = lastname;
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+
---
+++ src/main/java/com/sda/springstarter/demo/model/Publisher.java
@@ -0,0 +1,50 @@
+package com.sda.springstarter.demo.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Publisher {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String address;
+    private String name;
+
+    public Publisher() {
+    }
+
+    public Publisher(String address, String name) {
+        this.address = address;
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+
