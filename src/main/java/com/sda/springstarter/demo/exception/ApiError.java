f0155f491e641b2dcc443540c5da7e2139b2c239
AhlenSDA <ahlensda@wp.pl>
2018-01-14 11:45:22 +0100

    Modify ApiError class

--- src/main/java/com/sda/springstarter/demo/exception/ApiError.java
+++ src/main/java/com/sda/springstarter/demo/exception/ApiError.java
@@ -2,6 +2,7 @@ package com.sda.springstarter.demo.exception;
 
 import org.springframework.http.HttpStatus;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class ApiError {
@@ -11,30 +12,23 @@ public class ApiError {
     private List<String> errors;
 
     public ApiError(HttpStatus status, String message, List<String> errors) {
-        super();
         this.status = status;
         this.message = message;
         this.errors = errors;
     }
 
-    public HttpStatus getStatus() {
-        return status;
+    public ApiError(HttpStatus status, String message, String error) {
+        this.status = status;
+        this.message = message;
+        this.errors = Arrays.asList(error);
     }
 
     public void setStatus(HttpStatus status) {
         this.status = status;
     }
 
-    public String getMessage() {
-        return message;
-    }
-
     public void setMessage(String message) {
         this.message = message;
-    }
-
-    public List<String> getErrors() {
-        return errors;
     }
 
     public void setErrors(List<String> errors) {
